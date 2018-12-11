c212bf4c7c05d81385dcc9b882e5a98c9448c7d2
Rohit <roniepatil@gmail.com>
2018-12-11 17:13:11 +0530

    11th 5:15

--- js/main.js
+++ js/main.js
@@ -98,5 +98,49 @@ $(document).ready(function() {
       responsiveClass: true,
       responsive: { 0: { items: 1 }, 768: { items: 2 }, 900: { items: 4 } }
     });
+  
 
+    $('#pagination-demo').twbsPagination({
+      totalPages: 3,
+      // the current page that show on start
+      startPage: 1,
+       
+      // maximum visible pages
+      visiblePages: 3,
+       
+      initiateStartPageClick: true,
+       
+      // template for pagination links
+      href: false,
+       
+      // variable name in href template for page number
+      hrefVariable: '{{number}}',
+       
+      // Text labels
+      first: 'First',
+      prev: 'Previous',
+      next: 'Next',
+      last: 'Last',
+       
+      // carousel-style pagination
+      loop: false,
+       
+      // callback function
+      onPageClick: function (event, page) {
+         $('.page-active').removeClass('page-active');
+        $('#page'+page).addClass('page-active');
+      },
+       
+      // pagination Classes
+      paginationClass: 'pagination',
+      nextClass: 'next',
+      prevClass: 'prev',
+      lastClass: 'last',
+      firstClass: 'first',
+      pageClass: 'page',
+      activeClass: 'active',
+      disabledClass: 'disabled'
+       
+      });
+       
 });
